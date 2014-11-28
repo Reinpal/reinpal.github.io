f023d1ddf1f8ad034da337274edf4bd80206b21f
reinpal <stefanr92@gmx.at>
2014-11-28 20:20:27 +0100

    menu icon animation, homepage

--- js/site.js
+++ js/site.js
@@ -110,4 +110,21 @@
  		}
  	})
 
+ 		var anchor = document.querySelectorAll('.menu-icon');
+    
+    [].forEach.call(anchor, function(anchor){
+      var open = false;
+      anchor.onclick = function(event){
+        event.preventDefault();
+        if(!open){
+          this.classList.add('close');
+          open = true;
+        }
+        else{
+          this.classList.remove('close');
+          open = false;
+        }
+      }
+    }); 
+
 });
